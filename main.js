d6fc6a4caabfc9cdcbcec2ea30b2b936da56da77
bduke6 <duke.gard@gmail.com>
2022-04-27 19:21:06 -0600

    rendering rect to canvasPov
    Rendering to the canvasPov, text background work.

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 /// <reference path="https://cdn.babylonjs.com/babylon.js">
 
 //import * as BABYLON from "https://preview.babylonjs.com/babylon.js";
+//SSD Paper 2016 https://arxiv.org/pdf/1512.02325.pdf
 
 class BasicWorldDemo {
-
-    B
     constructor() {
       this._Initialize();
     }
@@ -18,12 +17,63 @@ class BasicWorldDemo {
         console.log("hello Babylon.js Canvases")
         const canvas = document.createElement('canvas') //notice this is a create element
         const canvasFollow = document.getElementById('renderCanvas0')
-        const canvasPov = document.getElementById('renderCanvas1')
+        this.canvasPov = document.getElementById('renderCanvas1')
+        this.canvas2D = document.getElementById('renderCanvas3')
+        this.canvas2D.width = this.canvasPov.width
+        this.canvas2D.height = this.canvasPov.height
+        this.children = [];//used for adding highlights
+        
+        // Load the model.
+        // this.model = cocoSsd.load().then(model=>{
+        //     console.log('Model is loaded', model.detect(this.canvasPov))
+        //     //this._RAF();
+        // });
+          // counter for testing
+          this.num = 0;
+          //Load the model.
+            cocoSsd.load().then(model=>{
+                this.model = model
+                console.log('Model loaded calling detect')
+                console.log(this.model)
+                //this._detect(model)
+                
+                this._RAF()
+            
+            });
 
-        // const ctx = canvasPov.getContext('2d');
-       
-        // ctx.fillStyle = 'red';
-        // ctx.fillRect(10, 10, 150, 100);
+            // model => {
+            //     //detect objects in the image.
+            //     model.detect(canvas2D).then(predictions => {
+            //     console.log('Predictions: ', predictions);
+            //     });
+
+        // Classify the image.
+        //const predictions =  model.detect('renderCanvas1');
+
+        //var canvas = document.getElementById('viewport'),
+        //code to text coco
+            // let context = this.canvas2D.getContext('2d');
+
+            //_make_base(){
+                // let base_image = new Image();
+                // base_image.src = 'cat.jpeg';
+                // base_image.onload = function(){
+                //     context.drawImage(base_image, 0, 0);
+                // }
+            //}
+
+        
+
+        //var c = document.createElement('renderCanvas1') 
+        this._ctx = this.canvasPov.getContext("2d");
+        this._ctx.globalAlpha = 0.3;
+        this._ctx.fillStyle = "rgba(0,255,0,.4)"
+        this._ctx.beginPath();
+        this._ctx.fillRect(20, 20, 150, 100);
+        this._ctx.strokeStyle = "rgba(81,255,13,.5)";
+        this._ctx.fillStyle = 'green' //"rgba(81,255,13,0)"
+        this._ctx.font = 'bold 90px Arial';
+        //this._ctx.fillText('Hello world', 20, 20);
 
         this._engine = new BABYLON.Engine(canvas,true);
 
@@ -255,6 +305,7 @@ class BasicWorldDemo {
         //Follow Camera attempt
         //this._camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(-6, 0, 0), this._aaascene);
         this._camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 10, -10), this._scene);
+        //this._camera.setPosition(new BABYLON.Vector3(0, 200, -30));
         this._camera.heightOffset = 8;
         this._camera.radius = 1;
         this._camera.rotationOffset = 0;
@@ -266,7 +317,7 @@ class BasicWorldDemo {
         this._camera1 = new BABYLON.UniversalCamera("Camera1",box3.position, this._scene); //new BABYLON.Vector3(0, .5, -1)
         // Set the camera's parent to mesh so it will continue to look at it. new BABYLON.Vector3(0, 5, -20)
         this._camera1.parent = box3;  //BABYLON.Vector3.Zero()
-        this._camera.attachControl(canvasPov);
+        this._camera.attachControl(this.canvasPov);
         
         
     
@@ -275,7 +326,7 @@ class BasicWorldDemo {
         this._light = new BABYLON.DirectionalLight("light1", new BABYLON.Vector3(-1, -3, 1), this._scene);
         this._light.position = new BABYLON.Vector3(3, 9, 3);
 
-        this._light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, -10, -2), this._scene);
+        this._light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), this._scene);
         this._light1.intensity = 0.2;
 
         this._shadowGenerator = new BABYLON.ShadowGenerator(1024, this._light);
@@ -283,18 +334,20 @@ class BasicWorldDemo {
         this._shadowGenerator.getShadowMap().renderList.push(this._box)
         this._shadowGenerator.getShadowMap().renderList.push(box3)
 
-        for (let x = -8; x < 8; x++) {
-            for (let y = -8; y < 8; y++) {
-              var box = new BABYLON.Mesh.CreateBox("box",'2',this._scene);
-                box.position.x = Math.random() + x * 5;
-                box.position.y = Math.random() * 8.0 + 2.0;
-                box.position.z = Math.random() + y * 5;
-                var boxMaterial = new BABYLON.StandardMaterial("material", this._scene);
-                boxMaterial.emissiveColor = new BABYLON.Color3(0, Math.random(), Math.random());//new BABYLON.Color3(0, 0.58, 0.86);
-                box.material = boxMaterial;
-                this._shadowGenerator.getShadowMap().renderList.push(box)
-            }
-          }
+        //const shadowGenerator = new BABYLON.ShadowGenerator(1024, this._light1);
+
+        // for (let x = -8; x < 8; x++) {
+        //     for (let y = -8; y < 8; y++) {
+        //       var box = new BABYLON.Mesh.CreateBox("box",'2',this._scene);
+        //         box.position.x = Math.random() + x * 5;
+        //         box.position.y = Math.random() * 8.0 + 2.0;
+        //         box.position.z = Math.random() + y * 5;
+        //         var boxMaterial = new BABYLON.StandardMaterial("material", this._scene);
+        //         boxMaterial.emissiveColor = new BABYLON.Color3(0, Math.random(), Math.random());//new BABYLON.Color3(0, 0.58, 0.86);
+        //         box.material = boxMaterial;
+        //         this._shadowGenerator.getShadowMap().renderList.push(box)
+        //     }
+        //   }
     
         // Default intensity is 1. Let's dim the light a small amount
         this._light.intensity = 0.7;
@@ -319,17 +372,20 @@ class BasicWorldDemo {
     
 
         this._engine.registerView(canvasFollow);
-        this._engine.registerView(canvasPov, this._camera1);
-    
-        this._RAF();
+        this._engine.registerView(this.canvasPov, this._camera1);
 
+        
+    
+        // this._RAF();
+
+            //})
     }
     
-    _OnWindowResize() {
+    _OnWindowResize(){
         this._engine.resize();
     }
 
-    _RAF() {
+    _RAF(){
         // requestAnimationFrame(() => {
         // this._threejs.render(this._scene, this._camera);
         // this._RAF();
@@ -337,22 +393,145 @@ class BasicWorldDemo {
         this._engine.runRenderLoop(() => {
             if(this._scene.activeCamera){
                 this._scene.render();
-
+                //this._writeText();
+                this._detect(this.model)
             }
-            
-        });
+        })
     
+    }
+
+
+
+    _writeText(predictions){
+
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * max);
+          }
+
+          for (let n = 0; n < predictions.length; n++) {
+            //console.log('Predictions: ', predictions[n].class, predictions[n].bbox, predictions[n].score);
+            if(predictions[n].score > 0.66){
+                
+          
+                //this._ctx.clearRect(0,0,400,400)
+                //let xx = getRandomInt(20)
+                //let yy = getRandomInt(20)
+                // let xx = parseInt(predictions[n].bbox[0]);
+                // let yy = parseInt(predictions[n].bbox[1]);
+                // this._ctx.strokeRect(xx, yy, 20, 20);
+                let bbox = predictions[n].bbox;
+                let x = parseInt(bbox[0]);
+                let y = parseInt(bbox[1]);
+                let w = parseInt(bbox[2]);
+                let h = parseInt(bbox[3]);
+                //this._ctx.fillStyle = "rgba(0,0,0,.9)"
+                
+                
+                
+                let font = 'bold 30px Arial'
+                this._ctx.font = font;
+                //"rgba(81,255,13,0)"
+                // this._ctx.fillStyle = "rgba(0,255,0,.4)"
+                // this._ctx.fill();
+                /// get width of text
+                let txt = predictions[n].class + " :" + predictions[n].score.toFixed(2);
+                let width = this._ctx.measureText(txt).width;
+                this._ctx.fillStyle = "rgba(0,255,0,.4)"
+                /// draw background rect assuming height of font
+                this._ctx.fillRect(x-10, y-60, width+20, 40);
+                //this._ctx.fillRect(x, y, 20, 30);
+                this._ctx.fillStyle = "rgba(81,255,13,.8)" //"rgba(0,255,0,.4)"
+                this._ctx.fillText(txt, x, y-30);
+                
+                
+                //   console.log('box x:',x)
+                //   console.log('box y:',y)
+                //   console.log('box w:',w)
+                //   console.log('box h:',h)
+                this._ctx.strokeStyle = "rgba(0,255,0,.4)"
+                this._ctx.lineWidth = 10;
+                this._ctx.strokeRect(x,y,w,h);
+                this._ctx.fillStyle = "rgba(0,255,0,.4)"
+                //this._ctx.fillRect(x,y - 60,w,30)
+            }    
+          }//end of for
+          
     }
 
     _LoadMesh(){
         // The first parameter can be used to specify which mesh to import. Here we import all meshes
-        BABYLON.SceneLoader.ImportMesh("", "astro/", "scene.gltf", this._scene, function (newMeshes) {
+        BABYLON.SceneLoader.ImportMesh("", "cat/", "scene.gltf", this._scene, function (newMeshes) {
         // Set the target of the camera to the first imported mesh
-        //camera.target = newMeshes[0];
+        newMeshes[0].position.x = 1
+        newMeshes[0].position.y = .25
+        newMeshes[0].position.z = 1
+        newMeshes[0].scaling.x = .25
+        newMeshes[0].scaling.y = .25
+        newMeshes[0].scaling.z = .25
+        //this._shadowGenerator.getShadowMap().renderList.push(newMeshes[0])
+        //shadowGenerator.addShadowCaster(newMeshes[0]);
     });
 
     }
-}
+
+    //coco detection
+    _detect(m){
+        //console.log('made it to _detect. now doing some stuff')
+        
+        m.detect(this.canvasPov).then(predictions => {
+            //console.log('Predictions: ', predictions[0].class, predictions[0].bbox, predictions[0].score);
+            let v = predictions[0].class;
+            // console.log("canvas wh", this.canvasPov.width,this.canvasPov.height)
+            // console.log("canvas2d wh", this.canvas2D.width,this.canvas2D.height)
+            //if(predictions[0].score > 0.6){
+                
+                this._writeText(predictions)
+                //}
+            })
+            .catch(error => {
+            
+            });
+        }
+
+    
+
+    // predictWebcam() {
+    // // Now let's start classifying a frame in the stream.
+    // model.detect(video).then(function (predictions) {
+    //     // Remove any highlighting we did previous frame.
+    //     for (let i = 0; i < children.length; i++) {
+    //     this.canvasPov.removeChild(this.children[i]);
+    //     }
+    //     this.children.splice(0);
+        
+    //     // Now lets loop through predictions and draw them to the live view if
+    //     // they have a high confidence score.
+    //     for (let n = 0; n < predictions.length; n++) {
+    //     // If we are over 66% sure we are sure we classified it right, draw it!
+    //         if (predictions[n].score > 0.66) {
+    //             const p = document.createElement('p');
+    //             p.innerText = predictions[n].class  + ' - with ' 
+    //                 + Math.round(parseFloat(predictions[n].score) * 100) 
+    //                 + '% confidence.';
+    //             p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+    //                 + (predictions[n].bbox[1] - 10) + 'px; width: ' 
+    //                 + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+    //             const highlighter = document.createElement('div');
+    //             highlighter.setAttribute('class', 'highlighter');
+    //             highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+    //                 + predictions[n].bbox[1] + 'px; width: ' 
+    //                 + predictions[n].bbox[2] + 'px; height: '
+    //                 + predictions[n].bbox[3] + 'px;';
+
+    //             this.canvasPov.appendChild(highlighter);
+    //             this.canvasPov.appendChild(p);
+    //             children.push(highlighter);
+    //             children.push(p);
+    //         }
+    //     }
+
+    }
 
 // class AxisArrows{
     
@@ -399,6 +578,7 @@ class BasicWorldDemo {
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('renderCanvas0').click();
     console.log("DOM content is loaded!!")
     _APP = new BasicWorldDemo();
 }); 
